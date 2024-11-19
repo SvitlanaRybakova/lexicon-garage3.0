343c12f2d07be0fc1803f0a471676afe387ca83a
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-19 19:37:52 +0100

    feat: implement the view for parking lot list

---
+++ lexicon-garage3.Web/Models/ViewModels/ParkingSpotsViewModels/IndexParkingSpotViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lexicon_garage3.Web.Models.ViewModels.ParkingSpotsViewModels
+{
+    public class IndexParkingSpotViewModel
+    {
+        public string Id { get; set; }
+
+        [Display(Name = "Space Size")]
+        public string Size { get; set; }
+
+        [Display(Name = "Space Number")]
+        public int ParkingNumber { get; set; }
+
+        [Display(Name = "Is Available")]
+        public bool IsAvailable { get; set; }
+
+        [Display(Name = "Hourly Rate")]
+        public int HourRate { get; set; }
+    }
+}
