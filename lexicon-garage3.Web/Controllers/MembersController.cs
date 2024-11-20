bfd5a7f922a1114a21cf242563ba593733237293
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-20 21:33:06 +0100

    feat: implement member details view with all information about vehicle, parking place, price

--- lexicon-garage3.Web/Controllers/MembersController.cs
+++ lexicon-garage3.Web/Controllers/MembersController.cs
@@ -22,26 +22,57 @@ namespace lexicon_garage3.Web.Controllers
         // GET: Members
         public async Task<IActionResult> IndexMembers()
         {
-            var members = await _context.Member.ToListAsync();
-            return View("Index", members);
+            var members = _context.Member
+             .Include(m => m.Vehicles)
+             .ThenInclude(v => v.ParkingSpot)
+             .ToList();
+
+            var memberViewModels = members.Select(m => new IndexMemberViewModel
+            {
+                Id = m.Id,
+                FullName = $"{m.FirstName} {m.LastName}",
+                VehicleCount = m.Vehicles.Count,
+                TotalParkingCost = m.Vehicles.Sum(v => CalculateParkingCost(v.ParkingSpot, v.ArrivalTime, v.CheckoutTime))
+            }).ToList();
+
+            return View("Index", memberViewModels);
         }
+
 
         // GET: Members/Details/5
         public async Task<IActionResult> Details(string id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            var member = _context.Member
+         .Include(m => m.Vehicles)
+         .ThenInclude(v => v.ParkingSpot)
+         .FirstOrDefault(m => m.Id == id);
 
-            var member = await _context.Member
-                .FirstOrDefaultAsync(m => m.Id == id);
             if (member == null)
             {
                 return NotFound();
             }
 
-            return View(member);
+            var viewModel = new DetailsMemberViewModel
+            {
+                Id = member.Id,
+                FullName = $"{member.FirstName} {member.LastName}",
+                UserName = member.UserName,
+                PersonNumber = member.PersonNumber,
+                Vehicles = member.Vehicles.Select(v => new VehicleDetailsViewModel
+                {
+                    RegNumber = v.RegNumber,
+                    Brand = v.Brand,
+                    Model = v.Model,
+                    Color = v.Color,
+                    ArrivalTime = v.ArrivalTime,
+                    CheckoutTime = v.CheckoutTime,
+                    ParkingCost = CalculateParkingCost(v.ParkingSpot, v.ArrivalTime, v.CheckoutTime),
+                    ParkingSpotName = v.ParkingSpot?.RegNumber,
+                    CostPerHour = v.ParkingSpot?.HourRate ?? 0
+                }).ToList()
+            };
+
+            return View(viewModel);
         }
 
         // GET: Members/Create
@@ -153,6 +184,14 @@ namespace lexicon_garage3.Web.Controllers
         private bool MemberExists(string id)
         {
             return _context.Member.Any(e => e.Id == id);
+        }
+
+        private decimal CalculateParkingCost(ParkingSpot parkingSpot, DateTime arrivalTime, DateTime checkoutTime)
+        {
+            var parkingDuration = checkoutTime - arrivalTime;
+            var hoursParked = (decimal)parkingDuration.TotalHours;
+
+            return hoursParked * (parkingSpot?.HourRate ?? 0); 
         }
     }
 }
