56e83e365ff9deea5fb24bcc12e2864e6c59f27c
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-21 15:30:29 +0100

    feat: create the person number validation

---
+++ lexicon-garage3.Web/Validation/PersonNumberValidationAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace lexicon_garage3.Web.Validation
+{
+    public class PersonNumberValidationAttribute : ValidationAttribute
+    {
+        private const string Pattern = @"^\d{8}-\d{4}$";
+
+        public PersonNumberValidationAttribute()
+        {
+            this.ErrorMessage = "The Person Number must be in the format YYYYMMDD-XXXX.";
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+  
+            string personNumber = value.ToString();
+            if (!Regex.IsMatch(personNumber, Pattern))
+            {
+                return new ValidationResult(ErrorMessage ?? "Invalid Person Number format.");
+            }
+
+            return ValidationResult.Success; 
+        }
+    }
+}
