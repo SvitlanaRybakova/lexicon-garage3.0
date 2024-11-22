4b50fe252b5cf806889bd2d3351cf850fd44c630
Maureen <maureendelafuente.b@gmail.com>
2024-11-22 16:37:20 +0100

    Receipt

---
+++ lexicon-garage3.Web/Models/ViewModels/VehicleViewModels/ParkingReceiptViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using lexicon_garage3.Core.Entities;
+
+namespace lexicon_garage3.Web.Models.ViewModels.VehicleViewModels;
+
+public class ParkingReceiptViewModel
+{
+    public string RegNumber { get; set; }
+
+    public string Color { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public DateTime ArrivalTime { get; set; }
+    public DateTime CheckoutTime { get; set; }
+
+    public VehicleType VehicleType { get; set; }
+    public ParkingSpot ParkingSpot { get; set; } 
+
+}
