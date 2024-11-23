e572bc812fcb3c3d9b304ac57a63ac16f8708b72
Maureen <maureendelafuente.b@gmail.com>
2024-11-24 00:14:55 +0100

    Add todo comment about possible bug (ParkingSpot allows duplicate ParkingNumbers)

--- lexicon-garage3.Core/Entities/ParkingSpot.cs
+++ lexicon-garage3.Core/Entities/ParkingSpot.cs
@@ -10,7 +10,7 @@ namespace lexicon_garage3.Core.Entities
     {
         public string Id { get; set; } 
         public string Size { get; set; }
-        public int ParkingNumber { get; set; }
+        public int ParkingNumber { get; set; } //TODO: maybe this should be unique? right now it's possible to have several spots with the same parkingnumber
         public bool IsAvailable { get; set; }
         public int HourRate { get; set; }
 
